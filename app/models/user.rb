82bdd3aa8cd2bfaebee4a99584a102f9b2491e29
Xander Miller <xander.miller@gmail.com>
2016-06-20 14:52:36 +0000

    fixes secure_password conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  before_save { self.email = email.downcase if email.present? }
 
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
-  validates :password, length: { minimum: 6 }, allow_blank: true
-  validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
-             
-  has_secure_password
+
 end
