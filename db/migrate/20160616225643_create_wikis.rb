f13e1d79ea7263852a097c4e011521c2c9e00f71
Xander Miller <xander.miller@gmail.com>
2016-07-13 19:49:42 +0000

    fixes wiki migration

--- db/migrate/20160616225643_create_wikis.rb
+++ db/migrate/20160616225643_create_wikis.rb
@@ -1,6 +1,10 @@
 class CreateWikis < ActiveRecord::Migration
   def change
     create_table :wikis do |t|
+      
+      t.boolean     :private, default: false, null: false
+      t.text        :body
+      t.references  :user, index: true, foreign_key: true, null: false
 
       t.timestamps null: false
     end
