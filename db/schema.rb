ec39e45e02a2c3c14893312d91b309837dc0c39e
Xander Miller <xander.miller@gmail.com>
2016-07-13 19:51:07 +0000

    updates scheme with new wiki

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,13 @@ ActiveRecord::Schema.define(version: 20160616225643) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "wikis", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "private",    default: false, null: false
+    t.text     "body"
+    t.integer  "user_id",                    null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
+
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
